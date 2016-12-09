795684b3e9d555d09d44ef9678c303d7bd64f782
my <my@mydeMacBook-Pro-2.local>
2016-12-09 15:04:52 +0800

    newproject
    creat a new project

---
+++ MYLocalStorage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MYLocalStorage
+//
+//  Created by my on 2016/12/9.
+//  Copyright © 2016年 my. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MYLocalStorage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MYLocalStorage
+//
+//  Created by my on 2016/12/9.
+//  Copyright © 2016年 my. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
